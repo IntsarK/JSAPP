8fa95c6f6862f1c46177ace7153a1ac75a3ebd47
IntsarK <162660098+IntsarK@users.noreply.github.com>
2025-01-24 07:23:51 -0600

    from type to types

--- js/scripts.js
+++ js/scripts.js
@@ -1,66 +1,65 @@
 let pokemonRepository = (function () {
-     let pokemonList = [
-     {
-          name: "Butterfree",
-          height: 1.1,
-          type: ["Bug", "Flying"], 
-     },
-     {
-          name:"Wigglytuff",
-          height: 1,
-          type: ["Fairy", "Normal"],
-     },
-     {
-          name: "Mewtwo",
-          height: 2,
-          type: ["Psychic"],
-     },
-     ];
+  let pokemonList = [
+    {
+      name: "Butterfree",
+      height: 1.1,
+      types: ["Bug", "Flying"],
+    },
+    {
+      name: "Wigglytuff",
+      height: 1,
+      types: ["Fairy", "Normal"],
+    },
+    {
+      name: "Mewtwo",
+      height: 2,
+      types: ["Psychic"],
+    },
+  ];
 
-     function getAll() {
-          return pokemonList;
-     }
-
-     function add(pokemon) {
-          if (
-               typeof pokemon === "object" &&
-               Object.keys(pokemon).length === 3 && 
-               Object.keys(pokemon).every((key) =>
-                 ["name", "height", "type"].includes(key)
-          )
-          ) {
-               pokemonList.push(pokemon); 
-             } else  {
-                console.error("Invalid Pokémon object!");
+  function getAll() {
+    return pokemonList;
   }
-}
 
-function find(name){
-     return pokemonList.filter(
-          pokemon => pokemon.name.toLowerCase() === name.toLowerCase()
-     );
-}
+  function add(pokemon) {
+    if (
+      typeof pokemon === "object" &&
+      Object.keys(pokemon).length === 3 &&
+      Object.keys(pokemon).every((key) =>
+        ["name", "height", "types"].includes(key)
+      )
+    ) {
+      pokemonList.push(pokemon);
+    } else {
+      console.error("Invalid Pokémon object!");
+    }
+  }
 
-function addListItem(pokemon){
-     let pokemonList = document.querySelector(".pokemon-list");
-     let listpokemon = document.createElement("li");
-     let button = document.createElement("button");
-     button.innerText = pokemon.name;
-     button.classList.add("button-class");
-     listItem.appendChild("button");
-     pokemonList.appendChild(listpokemon);
-}
-     return {
-          getAll: getAll,
-          add: add,
-          find: find,
-          addListItem: addListItem,
-     };
+  function find(name) {
+    return pokemonList.filter(
+      (pokemon) => pokemon.name.toLowerCase() === name.toLowerCase()
+    );
+  }
+
+  function addListItem(pokemon) {
+    let pokemonList = document.querySelector(".pokemon-list");
+    let listpokemon = document.createElement("li");
+    let button = document.createElement("button");
+    button.innerText = pokemon.name;
+    button.classList.add("button-class");
+    listItem.appendChild(button);
+    pokemonList.appendChild(listpokemon);
+  }
+  return {
+    getAll: getAll,
+    add: add,
+    find: find,
+    addListItem: addListItem,
+  };
 })();
 
 console.log(pokemonRepository.getAll());
 
-
-pokemonRepository.getAll().forEach(function(pokemon) {
-     pokemonRepository.addListItem(pokemon);
+pokemonRepository.getAll().forEach(function (pokemon) {
+  pokemonRepository.addListItem(pokemon);
 });
